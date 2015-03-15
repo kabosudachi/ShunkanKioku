8320599cf20fcd8c6b157be394b5a448c2051183
西村真奈 <nishimuramana@nishimuramana-no-MacBook-Pro.local>
2015-03-15 11:59:45 +0900

    Initial Commit

---
+++ shunkan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shunkan
+//
+//  Created by 西村真奈 on 2015/03/15.
+//  Copyright (c) 2015年 ManaNishimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shunkan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  shunkan
+//
+//  Created by 西村真奈 on 2015/03/15.
+//  Copyright (c) 2015年 ManaNishimura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
